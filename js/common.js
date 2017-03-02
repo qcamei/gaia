4146db5dfc318b82d746af9c4314b39ec555ad4b
孙艳 <sunyan@sunyans-MacBook-Pro.local>
2017-03-02 10:14:55 +0800

    更新小林的
    更新小林的

--- js/common.js
+++ js/common.js
@@ -1,7 +1,7 @@
 
 var _ip = location.origin;
 console.log(_ip.indexOf('localhost'));
-if(_ip.indexOf('localhost') <= -1){
+if(_ip.indexOf('localhost') >= -1){
     _ip = 'http://121.42.187.170';
 }
 
