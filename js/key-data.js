1dbdfdbb7f61697a4043d56ea9c07436209b00e3
孙艳 <sunyan@sunyans-MacBook-Pro.local>
2017-08-10 17:00:30 +0800

    修改关键数据

--- js/key-data.js
+++ js/key-data.js
@@ -68,160 +68,21 @@ setVisitOption = {
     chart.setOption(opt)
   }
 }
+
+//拜访
 setVisitOption.drawChart(echarts.init(document.getElementById('visit-chart-1')));
 setVisitOption.drawChart(echarts.init(document.getElementById('visit-chart-2')));
 setVisitOption.drawChart(echarts.init(document.getElementById('visit-chart-3')));
 
+//录入和完善
 setVisitOption.drawChart(echarts.init(document.getElementById('entry-perfecting-chart-1')));
 setVisitOption.drawChart(echarts.init(document.getElementById('entry-perfecting-chart-2')));
 
+//开户
+setVisitOption.drawChart(echarts.init(document.getElementById('open-account-1')));
+setVisitOption.drawChart(echarts.init(document.getElementById('open-account-2')));
 
-var chartUseChart = echarts.init(document.getElementById('use-chart'));
-option_use_chart = {
-  color: ['#3398DB'],
-  tooltip : {
-    trigger: 'axis',
-    axisPointer : {            // 坐标轴指示器，坐标轴触发有效
-      type : 'shadow'        // 默认为直线，可选为：'line' | 'shadow'
-    }
-  },
-  grid: {
-    left: '3%',
-    right: '4%',
-    bottom: '3%',
-    containLabel: true
-  },
-  xAxis : [
-    {
-      type : 'category',
-      data : ['报修', '接修', '预防性维护', '日常检查', '工程师巡检', '计量'],
-      axisTick: {
-        alignWithLabel: true
-      }
-    }
-  ],
-  yAxis : [
-    {
-      type : 'value'
-    }
-  ],
-  series : [
-    {
-      name:'维护次数',
-      type:'bar',
-      barWidth: '60%',
-      data:[10, 52, 200, 334, 390, 330]
-    }
-  ]
-};
-chartUseChart.setOption(option_use_chart)
-
-var chartOpenAccount_1 = echarts.init(document.getElementById('open-account-1'));
-option_open_account_1 = {
-  tooltip: {
-    trigger: 'item',
-    formatter: "{a} <br/>{b}: {c} ({d}%)"
-  },
-  series: [
-    {
-      name:'访问来源',
-      type:'pie',
-      selectedMode: 'single',
-      radius: [0, '30%'],
-
-      label: {
-        normal: {
-          position: 'inner'
-        }
-      },
-      labelLine: {
-        normal: {
-          show: false
-        }
-      },
-
-    },
-    {
-      name:'访问来源',
-      type:'pie',
-      radius: ['40%', '55%'],
-
-      data:[
-        {
-          value:335,
-          name:'直达',
-          labelLine: {
-            normal: {
-              show: false
-            }
-          },
-          itemStyle:{
-            normal:{
-              color:'#4d9be5'
-            }
-          }
-        },
-        {value:30, name:'邮件营销'},
-        {value:10, name:'其他'}
-      ]
-    }
-  ]
-};
-chartOpenAccount_1.setOption(option_open_account_1)
-
-var chartOpenAccount_2 = echarts.init(document.getElementById('open-account-2'));
-option_open_account_2 = {
-  tooltip: {
-    trigger: 'item',
-    formatter: "{a} <br/>{b}: {c} ({d}%)"
-  },
-  series: [
-    {
-      name:'访问来源',
-      type:'pie',
-      selectedMode: 'single',
-      radius: [0, '30%'],
-
-      label: {
-        normal: {
-          position: 'inner'
-        }
-      },
-      labelLine: {
-        normal: {
-          show: false
-        }
-      },
-
-    },
-    {
-      name:'访问来源',
-      type:'pie',
-      radius: ['40%', '55%'],
-
-      data:[
-        {
-          value:335,
-          name:'直达',
-          labelLine: {
-            normal: {
-              show: false
-            }
-          },
-          itemStyle:{
-            normal:{
-              color:'#4d9be5'
-            }
-          }
-        },
-        {value:30, name:'邮件营销'},
-        {value:10, name:'其他'}
-      ]
-    }
-  ]
-};
-chartOpenAccount_2.setOption(option_open_account_2)
-
+//开户的折线
 drawline = {
   getW:function () {
     var w = $('#accountBox').width()*0.6;
@@ -261,8 +122,50 @@ drawline = {
     ctx.stroke();
   }
 }
-
 drawline.lineTurn('lineTop_1')
 drawline.lineTurn('lineTop_2')
+
+
+//使用
+var chartUseChart = echarts.init(document.getElementById('use-chart'));
+option_use_chart = {
+  color: ['#4d9be5'],
+  tooltip : {
+    trigger: 'axis',
+    axisPointer : {            // 坐标轴指示器，坐标轴触发有效
+      type : 'shadow'        // 默认为直线，可选为：'line' | 'shadow'
+    }
+  },
+  grid: {
+    left: '3%',
+    right: '4%',
+    bottom: '3%',
+    containLabel: true
+  },
+  xAxis : [
+    {
+      type : 'category',
+      data : ['报修', '接修', '预防性维护', '日常检查', '工程师巡检', '计量'],
+      axisTick: {
+        alignWithLabel: true
+      }
+    }
+  ],
+  yAxis : [
+    {
+      type : 'value'
+    }
+  ],
+  series : [
+    {
+      name:'维护次数',
+      type:'bar',
+      barWidth: '20%',
+      data:[10, 52, 200, 334, 390, 330]
+    }
+  ]
+};
+chartUseChart.setOption(option_use_chart)
+
 
 
