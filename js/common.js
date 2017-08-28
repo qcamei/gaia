222b6d1b58653b161a2bdf85131e67f050389fe2
胡海 <huhai@huhaideMacBook-Pro.local>
2017-08-28 13:47:42 +0800

    remove feature of operation report

--- js/common.js
+++ js/common.js
@@ -148,23 +148,23 @@ function creatAsideDom() {
                         src:'doc-manage'
                     }
                  ]
-        },
-        runReport:{
-            name:'运营报告',
-            iconClass:'report',
-            data:[
-              {
-                id:'keydata',
-                name:'数据概览',
-                src:'key-data'
-              },
-              {
-                id:'dataexport',
-                name:'数据导出',
-                src:'data-export'
-              }
-            ]
         }
+        // runReport:{
+        //     name:'运营报告',
+        //     iconClass:'report',
+        //     data:[
+        //       {
+        //         id:'keydata',
+        //         name:'数据概览',
+        //         src:'key-data'
+        //       },
+        //       {
+        //         id:'dataexport',
+        //         name:'数据导出',
+        //         src:'data-export'
+        //       }
+        //     ]
+        // }
     }
     var h = ''
     for(var k in obj){
@@ -207,11 +207,11 @@ function modityAsideCurrent(){
         },      //系统配置文件列表
         documentmanage:{
             docmanage:['doc-manage']
-        },   //文档管理文件列表
-        runReport:{
-            keydata:['key-data','tend-count'],
-            dataexport:['data-export']
-        } //运营报告列表
+        }   //文档管理文件列表
+        // runReport:{
+        //     keydata:['key-data','tend-count'],
+        //     dataexport:['data-export']
+        // } //运营报告列表
 
     }
 
